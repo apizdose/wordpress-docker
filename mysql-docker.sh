2ad8f7907776f190ebe6bc83c4e05933e76c2c13
Duff <59646857+apizdose@users.noreply.github.com>
2022-02-16 12:47:40 +0200

    Update mysql-docker.sh

--- mysql-docker.sh
+++ mysql-docker.sh
@@ -1,3 +1,7 @@
+#Generate ssl certs
+
+sudo openssl req -x509 -nodes -days 365000 -newkey rsa:2048 -keyout ./nginx-selfsigned.key -out ./nginx-selfsigned.crt
+
 # Backup
 docker exec CONTAINER /usr/bin/mysqldump -u root --password=root DATABASE > backup.sql
 
